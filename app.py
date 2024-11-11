883179fdff9de3a0f874d509fe151f4a58c624e4
HanKun113 <KHAN009@e.ntu.edu.sg>
2024-11-11 04:04:17 +0000

    1

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask
-from flask import render_template, request
+#
+from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
-app.route("/",method=["GET","POST"])
+@app.route("/",methods=["GET","POST"])
 def index():
+    if request.method == "POST":
+        a = request.form['options']
+        print(a)
     return(render_template("index.html"))
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     app.run()
+
 
 
